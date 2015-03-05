b07d9e4e5ef1453379f9ce8efac44910cebe5e29
Brenton Strine <brenton.a.strine@jpl.nasa.gov>
2015-03-04 18:46:17 -0800

    protecting against magnification

--- js/controllers.js
+++ js/controllers.js
@@ -2,10 +2,14 @@
 var myApp = angular.module('myApp', []);
 
 //here's a controller
-myApp.controller('MyController', function MyController($scope, $http){
-
-	$http.get('js/people.json').success(function(data){
-		//here's a model
-		$scope.people = data;
-	});
-});
+myApp.controller('MyController', [
+		'$scope',
+		'$http',
+		function ($scope, $http){
+			$http.get('js/people.json').success(function(data){
+				//here's a model
+				$scope.people = data;
+			})
+		}
+	]
+);
